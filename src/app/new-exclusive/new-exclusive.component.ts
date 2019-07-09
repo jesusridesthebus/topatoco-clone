a949c2e72a8c3a3b4b2bacffe71498927e5574e4
Liz <jesusridesthebus@gmail.com>
2019-07-08 20:17:43 -0700

    deploy to firebase

--- src/app/new-exclusive/new-exclusive.component.ts
+++ src/app/new-exclusive/new-exclusive.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+// import { NewExclusive } from '../../newexclusive.model';
 
 @Component({
   selector: 'app-new-exclusive',
   templateUrl: './new-exclusive.component.html',
   styleUrls: ['./new-exclusive.component.css']
 })
+
 export class NewExclusiveComponent implements OnInit {
+  // newExclusiveId: string;
+  // newExclusiveToDisplay: NewExclusive;
 
   constructor() { }
 
---
+++ src/newexclusive.model.ts
@@ -0,0 +1,6 @@
+export class NewExclusive {
+  constructor (
+    public title: string,
+    public price: string,
+    public image: string) {} //????
+}
